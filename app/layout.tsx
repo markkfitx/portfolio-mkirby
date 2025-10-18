b19354f4f658de1e37d36d649095e413e75c4740
Mark Kirby <markkirby1997@gmail.com>
2025-10-18 18:32:13 -0400

    Fixed template routing issue by switching layout and page.tsx. Added the subpage files.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/app/components/header"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,16 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}>
-        {children}
+        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+          {/** Navigation */}
+          <Header />
+          {/** Main withg children pages */}
+          <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+            {children}
+          </main>
+          {/** Footer */}
+          <footer></footer>
+        </div>
       </body>
     </html>
   );
