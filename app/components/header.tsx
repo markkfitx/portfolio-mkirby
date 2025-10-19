b99d8a160f0fba8fa62c6871a63fb101accfd6ed
Mark Kirby <markkirby1997@gmail.com>
2025-10-18 20:53:04 -0400

    structure changes.

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -8,8 +8,8 @@ import avatar from "@/img/avatar.jpg"
 
 export default function Header(){
     return(
-        <div className="container-fluid py-8 flex flex-row justify-between content-center w-full">
-            <h1 className="text-sm text-white font-bold uppercase m-0 leading-4">Mark<br/>Kirby</h1>
+        <div className="container-fluid flex flex-row justify-between content-center w-full">
+            <span className="text-sm text-white font-bold uppercase m-0 leading-3">Mark<br/>Kirby</span>
             {/*<div className="flex flex-row justify-start content-center gap-3 avatar-wrapper">
                 <Avatar className="shadow-sm border-1 border-gray-100 h-7 w-7 opacity-75">
                     <AvatarImage src={avatar.src} alt="Avatar Image"/>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-
+import Hero from "@/app/components/hero/hero"
 
 export default function Home({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
-    <></>
+    <Hero/>
   );
 }
